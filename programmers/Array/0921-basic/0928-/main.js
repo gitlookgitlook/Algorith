3e4e38ca90f547a3f48974e1e243fdb5dc665471
eunjinim <eunjee2058@gmail.com>
2024-09-29 10:43:42 +0900

    array.filter() - 09.29일.일요일

--- programmers/Array/0921-basic/0928-/main.js
+++ programmers/Array/0921-basic/0928-/main.js
@@ -10,9 +10,9 @@ console.log(newNumbers1) // 15
 
 // 문제 2: 배열 중 최대값 찾기(Math.max() + apply() 또는 reduce() 사용)
 // let numbers = [10, 3, 76, 34, 23];  // 결과: 76
-let number2 = [10, 3, 76, 34, 23]
+let numbers2 = [10, 3, 76, 34, 23]
 // 1. 내가 푼 방법
-let maxNumber1 = number2.reduce((acc, cur) => {
+let maxNumber1 = numbers2.reduce((acc, cur) => {
   if (acc >= cur) {
     return acc
   } else {
@@ -21,11 +21,19 @@ let maxNumber1 = number2.reduce((acc, cur) => {
 })
 console.log(maxNumber1) // 76
 // 2. 제시해준 방법
-let maxNumber2 = number2.reduce((acc, cur) => Math.max(acc, cur))
+let maxNumber2 = numbers2.reduce((acc, cur) => Math.max(acc, cur))
 console.log(maxNumber2) // 76
+
 // 문제 3: 짝수만 걸러내기(filter() 사용)
 // let numbers = [1, 2, 3, 4, 5, 6, 7, 8];  // 결과: [2, 4, 6, 8]
+// filter()는 true면 반환해줌
+let numbers3 = [1, 2, 3, 4, 5, 6, 7, 8]
+let evenNumbers = numbers3.filter((number) => {
+  return number % 2 == 0
+})
 
+console.log(numbers3) // [1, 2, 3, 4, 5, 6, 7, 8]
+console.log(evenNumbers) // [2, 4, 6, 8]
 // 문제 4: 배열 뒤집기 (reverse() 사용)
 // let words = ["apple", "banana", "cherry"];  // 결과: ["cherry", "banana", "apple"]
 
